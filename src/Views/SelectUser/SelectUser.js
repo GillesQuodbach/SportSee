9be2c7660d2196174ae5b2ceff5ef825aecb45f7
GillesQuodbach <quodbach.gilles@gmail.com>
2023-02-21 13:00:08 +0100

    Mise en place projet

---
+++ src/Views/SelectUser/SelectUser.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './SelecteUser.css'
+
+export default function SelectUser() {
+  return (
+    <div>SelectUser</div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import {BrowserRouter, Routes, Route, } from 'react-router-dom'
+import Error404 from "./Views/Error404/Error404"
+import SelectUser from "./Views/SelectUser/SelectUser"
+import Dashboard from './Views/Dashboard/Dashboard';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-    <App />
+    <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="*" element={<Error404/>}/>
+        <Route path="/selectuser/:id" element={<SelectUser/>}/>
+    </Routes>
     </BrowserRouter>
 );
 
