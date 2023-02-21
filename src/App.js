649e4b64a4110edcc600a5cf45c791edbf594bbf
GillesQuodbach <quodbach.gilles@gmail.com>
2023-02-21 13:36:12 +0100

    Mise en place projet

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Components/Header/Header";
+import Dashboard from "./Views/Dashboard/Dashboard";
+import Footer from "./Components/Footer/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Dashboard />
+      <Footer />
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import {BrowserRouter, Routes, Route, } from 'react-router-dom'
 import Error404 from "./Views/Error404/Error404"
 import SelectUser from "./Views/SelectUser/SelectUser"
 import Dashboard from './Views/Dashboard/Dashboard';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
     <Routes>
+    <Route path="/" element={<App/>}>
         <Route path="/" element={<Dashboard/>}/>
         <Route path="*" element={<Error404/>}/>
         <Route path="/selectuser/:id" element={<SelectUser/>}/>
+        </Route>
     </Routes>
     </BrowserRouter>
 );
