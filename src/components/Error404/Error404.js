270c8ecc13d72ed4aa4a086f7c85774f9687e693
GillesQuodbach <quodbach.gilles@gmail.com>
2023-03-11 08:11:06 +0100

    Corrections

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -1,5 +1,11 @@
 import React from "react";
+import s from "./syle.module.css";
 
 export default function Error404() {
-  return <div>Error404</div>;
+  return (
+    <div className={s.error404_container}>
+      <p className={s.error404_text1}>Oups, il y a eu une erreur !</p>
+      <p className={s.error404_text2}>La page est introuvable.</p>
+    </div>
+  );
 }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./style.module.css";
 import yogaIcon from "../../assets/icons/yoga.svg";
 import swimIcon from "../../assets/icons/swimming.svg";
@@ -9,10 +10,18 @@ export default function Sidebar() {
   return (
     <div className={s.sidebar_container}>
       <div className={s.sidebar_icons_container}>
-        <img src={yogaIcon} alt="" />
-        <img src={swimIcon} alt="" />
-        <img src={bikeIcon} alt="" />
-        <img src={gymIcon} alt="" />
+        <NavLink to="#">
+          <img src={yogaIcon} alt="" />
+        </NavLink>
+        <NavLink to="#">
+          <img src={swimIcon} alt="" />
+        </NavLink>
+        <NavLink to="#">
+          <img src={bikeIcon} alt="" />
+        </NavLink>
+        <NavLink to="#">
+          <img src={gymIcon} alt="" />
+        </NavLink>
       </div>
       <p className={s.copyright}>Copyright. SportSee 2020</p>
     </div>
